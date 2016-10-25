0f7ba6eb27d23f9e3c29952967f066135dc7b880
Abhilash Sunkam <abhilashsunkam@Abhilashs-MacBook-Pro.local>
2016-10-25 16:35:39 +0530

    Generated war file

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,8 +13,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @SpringBootApplication
 @Configuration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
